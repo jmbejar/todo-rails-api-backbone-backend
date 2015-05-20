d889e38d69e03ce3e82c14e2c99236329c29af88
Jorge Bejar <jorge@wyeworks.com>
2015-05-20 11:06:04 -0300

    Initial backend commit

---
+++ config/initializers/wrap_parameters.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+
+# To enable root element in JSON for ActiveRecord objects.
+# ActiveSupport.on_load(:active_record) do
+#   self.include_root_in_json = true
+# end
