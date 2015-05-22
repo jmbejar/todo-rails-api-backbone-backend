93663abe77efb2e8dba7b541f399e70ed4e7fcd5
Jorge Bejar <jorge@wyeworks.com>
2015-05-22 16:28:10 -0300

    Try having a API controller but adding only needed stuff

--- app/controllers/subscriptors_controller.rb
+++ app/controllers/subscriptors_controller.rb
@@ -1,4 +1,39 @@
-class SubscriptorsController < ActionController::Base
+class SubscriptorsController < ApplicationController
+
+  # Modules that won't work in a rails app
+  #
+  # You could include them but not found methods or other errors will happen
+  # depper in the stack if some of this features are used (eg. flash method)
+
+  # include ActionController::Flash
+  # include ActionController::Cookies
+
+
+  # Modules not included in API Controllers that *can* be included if needed
+
+  #include AbstractController::Translation
+  #include AbstractController::AssetPaths
+  #include ActionController::Helpers
+
+  include ActionView::Layouts
+
+  #include ActionController::EtagWithTemplateDigest
+  #include ActionController::Caching
+  #include ActionController::MimeResponds
+
+  include ActionController::ImplicitRender
+
+  #include ActionController::FormBuilder
+
+  include ActionController::RequestForgeryProtection
+
+  #include ActionController::Streaming
+  #include ActionController::HttpAuthentication::Basic::ControllerMethods
+  #include ActionController::HttpAuthentication::Digest::ControllerMethods
+  #include ActionController::HttpAuthentication::Token::ControllerMethods
+  #include ActionController::ParamsWrapper
+
+  layout 'application'
 
   before_action :find_subscriptor, only: :unsubscribe
 
@@ -9,18 +44,20 @@ class SubscriptorsController < ActionController::Base
     if @subscriptor
       @error = "Something went wrong" unless @subscriptor.destroy
     else
-      @error = "Email #{email_param} not found"
+      @error = "Email #{subscriptor_params[:email]} not found"
     end
+
+    render :unsubscribe_form if @error
   end
 
   private
 
   def find_subscriptor
-    @subscriptor = Subscriptor.find_by_email(email_param)
+    @subscriptor = Subscriptor.find_by_email(subscriptor_params[:email])
   end
 
   # Only allow a trusted parameter "white list" through.
-  def email_param
-    params.require(:subscriptor).require(:email)
+  def subscriptor_params
+    params.require(:subscriptor).permit(:email)
   end
 end
