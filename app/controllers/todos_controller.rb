0bc7dfc28edc6dc79edb57a7715020135d384ed8
Jorge Bejar <jorge@wyeworks.com>
2015-06-05 16:00:45 -0300

    Update todos controller after scaffolding agian with jbuilder

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,42 +2,42 @@ class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :update, :destroy]
 
   # GET /todos
+  # GET /todos.json
   def index
     @todos = Todo.all
-
-    render json: @todos
   end
 
   # GET /todos/1
+  # GET /todos/1.json
   def show
-    render json: @todo
   end
 
   # POST /todos
+  # POST /todos.json
   def create
     @todo = Todo.new(todo_params)
 
     if @todo.save
-      render json: @todo, status: :created, location: @todo
+      render :show, status: :created, location: @todo
     else
       render json: @todo.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /todos/1
+  # PATCH/PUT /todos/1.json
   def update
     if @todo.update(todo_params)
-      render json: @todo
+      render :show, status: :ok, location: @todo
     else
       render json: @todo.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /todos/1
+  # DELETE /todos/1.json
   def destroy
     @todo.destroy
-
-    head :no_content
   end
 
   private
@@ -46,7 +46,7 @@ class TodosController < ApplicationController
       @todo = Todo.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
+    # Never trust parameters from the scary internet, only allow the white list through.
     def todo_params
       params.require(:todo).permit(:title, :completed, :order)
     end
